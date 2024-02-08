dec4d11b4dea6ad06ceef35d3a83f315f788d7ca
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-02-08 11:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+const email = document.getElementById("email");
+const job = document.getElementById("job");
+const number = document.getElementById("number");
+const submitBtn = document.getElementById("btn");
+
+localStorage.setItem(name, name.value);
+localStorage.setItem(age, age.value);
+localStorage.setItem(email, email.value);
+localStorage.setItem(job, job.value);
+localStorage.setItem(number, number.value);
+
+localStorage.getItem(name);
+localStorage.getItem(age);
+localStorage.getItem(email);
+localStorage.getItem(job);
+localStorage.getItem(number);
